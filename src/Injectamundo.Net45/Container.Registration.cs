b04f2d40afe8704b06862924aec911280d412929
Steven Testa <steve@testasoftware.com>
2016-01-28 17:33:00 -0500

    broke out the Container to a partial class

---
+++ src/Injectamundo.Net45/Container.Registration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Injectamundo
+{
+    partial class Container
+    {
+        public void Register<TService, TImplementation>()
+            where TImplementation : class, TService
+            where TService : class
+        {
+            var registration = new Registration(typeof(TService), typeof(TImplementation), new TransientLifeStyle());
+            // todo: check if a registration already exists
+            registrations.Add(registration);
+        }
+
+        internal Registration GetRegistration(Type serviceType)
+        {
+            var registration = registrations.SingleOrDefault(c => c.ServiceType == serviceType);
+
+            // todo: if registration is null and type is concrete return registration for the concrete type
+            //if (registrations == null && registration is concrete type )
+            // return new Registration of Concrete Type
+
+            return registration;
+        }
+    }
+}
---
+++ src/Injectamundo.Net45/Container.Resolution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Injectamundo
+{
+    partial class Container
+    {
+        public TService GetInstance<TService>()
+            where TService : class
+        {
+            var registration = registrations.Single(r => r.ServiceType == typeof(TService));
+            var lifestyle = registration.Lifestyle;
+            var instance = lifestyle.ProduceInstance(this, registration);
+
+            return instance as TService;
+        }
+    }
+}
